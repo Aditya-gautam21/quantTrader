7085828b44476a2de2ffb69d7371225208f4377f
Aditya-gautam21 <adigaur121@gmail.com>
2026-02-27 12:45:48 +0530

    practise

--- backend/training/lstm_prac.py
+++ backend/training/lstm_prac.py
@@ -4,6 +4,12 @@ import torch.nn as nn
 
 class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size):
-        self.input_size = input_size
-        self.hidden_size = hidden_size
+        super().__init__()
+
+        self.LSTM = nn.LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=1,
+            batch_first=True
+        )
 
