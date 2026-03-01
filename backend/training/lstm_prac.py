fac877d3055e1faf18f5c6c9e62cc1ca78b05e01
Aditya-gautam21 <adigaur121@gmail.com>
2026-03-01 14:46:04 +0530

    changes

--- backend/training/lstm_prac.py
+++ backend/training/lstm_prac.py
@@ -6,10 +6,25 @@ class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size):
         super().__init__()
 
-        self.LSTM = nn.LSTM(
+        self.lstm = nn.LSTM(
             input_size=input_size,
             hidden_size=hidden_size,
             num_layers=1,
             batch_first=True
         )
 
+        self.attention = nn.attention(
+
+        )
+
+
+        self.fc = nn.Sequential(
+            nn.Linear(),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward():
+        lstm_out, (hidden, cell) = self.lstm(x)
